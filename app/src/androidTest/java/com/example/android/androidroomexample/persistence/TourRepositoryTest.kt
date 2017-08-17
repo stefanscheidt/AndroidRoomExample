d8a7500295aedcfbd0fc9c4f48f5b4658ea68589
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2017-08-17 14:28:49 +0200

    expect constraint exception in insert-invalid-tour-test.

--- app/src/androidTest/java/com/example/android/androidroomexample/persistence/TourRepositoryTest.kt
+++ app/src/androidTest/java/com/example/android/androidroomexample/persistence/TourRepositoryTest.kt
@@ -1,12 +1,12 @@
 package com.example.android.androidroomexample.persistence
 
 import android.arch.persistence.room.Room
+import android.database.sqlite.SQLiteConstraintException
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import com.example.android.androidroomexample.model.Order
 import com.example.android.androidroomexample.model.Tour
 import org.junit.After
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,9 +33,8 @@ class TourRepositoryTest {
         tourDatabase.close()
     }
 
-    @Test
+    @Test(expected = SQLiteConstraintException::class)
     fun insertInvalidTour() {
-        val tour = Tour("tour-1", arrayListOf(Order("order-1"), Order("order-1")))
-        tourRepository.insertTour(tour)
+        tourRepository.insertTour(Tour("tour-1", arrayListOf(Order("order-1"), Order("order-1"))))
     }
 }
